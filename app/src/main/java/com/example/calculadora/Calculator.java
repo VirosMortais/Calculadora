f3c7454b40356d4e1e474c1c0fbd4ad38c0cd218
chaarrsan <charlestacodeouro@hotmail.com>
2023-10-17 19:27:25 +0200

    Hay que terminar la clase calc

--- app/src/main/java/com/example/calculadora/Calculator.java
+++ app/src/main/java/com/example/calculadora/Calculator.java
@@ -6,24 +6,41 @@ public class Calculator {
 
    public double calcular(List<String> oper){
 
-       for(int i = 0; i < oper.size(); i++)
-            if(oper.contains("*") || oper.contains("/")){
-                if( oper.get(i).equals("*") || oper.get(i).equals("/")) {
-                    String sw = oper.get(i);
-                    switch (sw) {
-                        case "*" :
-                            oper.set(i-1, Integer.toString(Integer.parseInt(oper.get(i-1)) * Integer.parseInt(oper.get(i+1))));
-                            oper.remove(i);
-                            oper.remove(i);
-                            break;
-                        case "/" :
-                            oper.add(String.valueOf(div(oper.get(i - 1), oper.get(i + 1))));
-                            oper.remove(i);
-                            oper.remove(i);
-                            break;
-                    }
-                }
-            }
+       for(int i = 0; i < oper.size(); i++) {
+           if (oper.contains("*") || oper.contains("/")) {
+               if (oper.get(i).equals("*") || oper.get(i).equals("/")) {
+                   String sw = oper.get(i);
+                   switch (sw) {
+                       case "*":
+                           oper.set(i - 1, Integer.toString(Integer.parseInt(oper.get(i - 1)) * Integer.parseInt(oper.get(i + 1))));
+                           oper.remove(i);
+                           oper.remove(i);
+                           break;
+                       case "/":
+                           oper.add(String.valueOf(div(oper.get(i - 1), oper.get(i + 1))));
+                           oper.remove(i);
+                           oper.remove(i);
+                           break;
+                   }
+               }
+           }
+       }
+       for(int i = 0; i < oper.size(); i++) {
+                   String sw = oper.get(i);
+                   switch (sw) {
+                       case "+":
+                           oper.set(i - 1, Integer.toString(Integer.parseInt(oper.get(i - 1)) * Integer.parseInt(oper.get(i + 1))));
+                           oper.remove(i);
+                           oper.remove(i);
+                           break;
+                       case "-":
+                           oper.add(String.valueOf(div(oper.get(i - 1), oper.get(i + 1))));
+                           oper.remove(i);
+                           oper.remove(i);
+                           break;
+
+           }
+       }
 
        return 0;
    }
