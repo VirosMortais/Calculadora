eee7f198d94595a39f09569d08af3fdb4e0affb1
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-18 14:24:15 +0200

    Codigo Documentado

--- app/src/main/java/com/example/calculadora/Calculator.java
+++ app/src/main/java/com/example/calculadora/Calculator.java
@@ -2,26 +2,49 @@ package com.example.calculadora;
 
 import java.util.List;
 
+/**
+ * Clase que realiza las operaciones de la calculadora
+ *
+ * @version 1.0
+ * @since 2.0
+ * @see MainActivity
+ *
+ * @author Charles Arruda Santos
+ * @date 12/03/2021
+ *
+ */
 public class Calculator {
 
+    /**
+     * Método que realiza las operaciones de la calculadora
+     * Comprueba si hay multiplicación o división, si hay, realiza la operación primero
+     * y elimina los elementos de la lista, dejando solo el resultado de la operación.
+     * Después comprueba si hay suma o resta, si hay, realiza la operación después de la
+     * multiplicación o división
+     *
+     * @param oper lista de operaciones
+     * @return int resultado de la operación
+     */
     public int calcular(List<String> oper) {
+        // Comprueba si hay multiplicación o división
         if (oper.contains("*") || oper.contains("/")) {
             for (int i = 0; i < oper.size(); i++) {
 
                 if (oper.get(i).equals("*") || oper.get(i).equals("/")) {
                     String sw = oper.get(i);
                     switch (sw) {
-                        case "*":
+                        case "*" -> {
                             oper.set(i - 1, Integer.toString(multi(oper.get(i - 1), oper.get(i + 1))));
                             oper.remove(i);
                             oper.remove(i);
                             i--;
-                            continue;
-                        case "/":
+                        }
+                        case "/" -> {
                             oper.set(i - 1, Integer.toString(div(oper.get(i - 1), oper.get(i + 1))));
                             oper.remove(i);
                             oper.remove(i);
                             i--;
+                        }
                     }
                 }
             }
@@ -30,35 +53,60 @@ public class Calculator {
         for (int i = 0; i < oper.size(); i++) {
             String sw = oper.get(i);
             switch (sw) {
-                case "+":
+                case "+" -> {
                     oper.set(i - 1, Integer.toString(sum(oper.get(i - 1), oper.get(i + 1))));
                     oper.remove(i);
                     oper.remove(i);
                     i--;
-                    continue;
-                case "-":
+                }
+                case "-" -> {
                     oper.set(i - 1, Integer.toString(res(oper.get(i - 1), oper.get(i + 1))));
                     oper.remove(i);
                     oper.remove(i);
                     i--;
+                }
             }
         }
-
+        // Devuelve el resultado de la operación
         return Integer.parseInt(oper.get(0));
     }
 
+    /**
+     * Método que realiza la multiplicación
+     * @param n1 String con los números a multiplicar
+     * @param n2 String con los números a multiplicar
+     * @return int resultado de la multiplicación
+     */
     public int multi(String n1, String n2) {
         return Integer.parseInt(n1) * Integer.parseInt(n2);
     }
 
+    /**
+     * Método que realiza la división
+     * @param n1 String con los números a dividir
+     * @param n2 String con los números a dividir
+     * @return int resultado de la división
+     */
     public int div(String n1, String n2) {
         return Integer.parseInt(n1) / Integer.parseInt(n2);
     }
 
+    /**
+     * Método que realiza la suma
+     * @param n1 String con los números a sumar
+     * @param n2 String con los números a sumar
+     * @return int resultado de la suma
+     */
     public int sum(String n1, String n2) {
         return Integer.parseInt(n1) + Integer.parseInt(n2);
     }
 
+    /**
+     * Método que realiza la resta
+     * @param n1 String con los números a restar
+     * @param n2 String con los números a restar
+     * @return int resultado de la resta
+     */
     public int res(String n1, String n2) {
         return Integer.parseInt(n1) - Integer.parseInt(n2);
     }
--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -11,16 +11,52 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que realiza las operaciones de la calculadora
+ * @version 2.0
+ * @since 2.0
+ * @see MainActivity
+ * @see Calculator
+ *
+ * @date 12/03/2021
+ * @author Charles Arruda Santos
+ *
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Lista que almacena los números y operadores ingresados por el usuario
+     */
+    private final List<String> calc = new ArrayList<>();
 
-    private List<String> calc = new ArrayList<>();
+    /**
+     * Instancia de la clase Calculator
+     */
     private final Calculator calculator = new Calculator();
 
+    /**
+     * Variable que almacena el número ingresado por el usuario
+     */
     private String valueNumber = "";
 
+    /**
+     * TextView que muestra el resultado de las operaciones
+     */
     private TextView resultView;
 
+    /**
+     * Called when the activity is starting.  This is where most initialization
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     * @see Calculator
+     * @see #onNumberButtonClick(View)
+     * @see #onOperationButtonClick(View)
+     * @see #isOperator(String)
+     * @see #onCreate(Bundle)
+     */
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,12 +127,20 @@ public class MainActivity extends AppCompatActivity {
             calc.add(valueNumber);
             valueNumber = "";
             resultView.setText(String.valueOf(calculator.calcular(calc)));
-
         });
 
     }
 
-    // Método para manejar la entrada de números
+    /**
+     * Método para manejar la entrada de números en la calculadora y mostrarlos en la pantalla
+     * Si el número es 0, el método no hace nada
+     * Si el número no es 0, el método agrega el número a la lista y lo muestra en la pantalla
+     * Si el número es 0 y la pantalla no está vacía, el método agrega el número a la lista y lo
+     * muestra en la pantalla (esto es para que el usuario pueda ingresar números como 10, 100, etc.)
+     *
+     * @param view vista del botón presionado
+     */
+    @SuppressLint("SetTextI18n")
     public void onNumberButtonClick(View view) {
         Button btn = (Button) view;
         if (!resultView.getText().toString().equals("0")) {
@@ -107,6 +151,21 @@ public class MainActivity extends AppCompatActivity {
         valueNumber += btn.getText().toString();
     }
 
+
+    /**
+     * Método para manejar la entrada de operadores
+     * El método verifica si el usuario está ingresando un nuevo operador o si está ingresando
+     * un operador después de un número
+     * Si el usuario está ingresando un nuevo operador, el método agrega el operador a la lista
+     * Si el usuario está ingresando un operador después de un número, el método agrega el número
+     * a la lista y luego agrega el operador
+     * Si el usuario está ingresando un operador después de un operador, el método no hace nada
+     *
+     * @param view vista del botón presionado
+     * @see #isOperator(String)
+     * @see #onNumberButtonClick(View)
+     */
+    @SuppressLint("SetTextI18n")
     public void onOperationButtonClick(View view) {
         Button button = (Button) view;
         String operante = button.getText().toString();
@@ -145,6 +204,12 @@ public class MainActivity extends AppCompatActivity {
         valueNumber = "";
     }
 
+    /**
+     * Método para verificar si un texto es un operador
+     *
+     * @param text texto a verificar
+     * @return boolean true si es un operador, false si no
+     */
     private boolean isOperator(String text) {
         return text.equals("+") || text.equals("-") || text.equals("*") || text.equals("/");
     }
