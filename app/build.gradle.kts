4ee2fce1ecba18b06e931daff75688b525e3fdc8
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-18 14:08:44 +0200

    Creado el test para el la clase Calculator.java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,14 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 
     testImplementation ("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params:5.10.0")
 
 }
 
--- app/src/test/java/com/example/calculadora/CalculatorTest.java
+++ app/src/test/java/com/example/calculadora/CalculatorTest.java
@@ -1,16 +1,33 @@
 package com.example.calculadora;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CalculatorTest {
-
-    @Test
-    void name() {
-        System.out.println("Teste 1");
+    @DisplayName("Testar la funcion calcular")
+    @ParameterizedTest
+    @CsvSource({
+            "2 * 3,6",
+            "1 * 2 * 8,16",
+            "2 * 2 + 3,7",
+            "3 + 2 * 2,7",
+            "3 + 2 * 2 + 4,11",
+    })
+    void testCalcular(String input, int expectedResult) {
+        Calculator calc = new Calculator();
+        List<String> lista = new ArrayList<>(Arrays.asList(input.split(" "))); // Convert the input to a mutable list
+        int result = calc.calcular(lista);
+        assertEquals(expectedResult, result);
     }
 
-}
+    }
+
