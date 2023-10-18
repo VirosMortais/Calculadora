613967898b814f77302c3f1333d749d20b6ce4aa
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-18 04:00:15 +0200

    Code refactorized

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private List<String> calc = new ArrayList<>();
-    private Calculator calculator = new Calculator();
+    private final Calculator calculator = new Calculator();
 
     private String valueNumber = "";
 
@@ -61,14 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Agrega el numero a la pantalla
         for (Button n : nums) {
-            n.setOnClickListener(view -> {
-                if (!resultView.getText().toString().equals("0")) {
-                    resultView.setText(resultView.getText().toString() + n.getText().toString());
-                } else {
-                    resultView.setText(n.getText().toString());
-                }
-                valueNumber += n.getText().toString();
-            });
+            n.setOnClickListener(this::onNumberButtonClick);
         }
 
         //Creacion de una lista con los simbolos
@@ -81,41 +75,7 @@ public class MainActivity extends AppCompatActivity {
         //Hago la operacion correspondiente si se presiona un simbolo y el primer numero es diferente de 0
         //Si el primer numero es 0, se guarda el numero en la variable firstNumber
         for (Button s : simbols) {
-            s.setOnClickListener(view -> {
-                String operante = s.getText().toString();
-                boolean invalid = false;
-
-                    if (calc.isEmpty()) {
-                        // Si la lista está vacía, agrega el número seguido del operador
-                        calc.add(valueNumber);
-                        calc.add(operante);
-                    } else if(!valueNumber.equals("")){
-                        String lastItem = calc.get(calc.size() - 1);
-                        if (isOperator(lastItem)) {
-                            calc.add(valueNumber);
-                            calc.add(operante);
-                        } else {
-                            // El último elemento no es un operador, por lo que el usuario debe estar ingresando un nuevo operador
-                            calc.add(operante);
-                            calc.add(valueNumber);
-                        }
-                    }else {
-                        // Verifica si el último elemento de la lista es un operador
-                        String lastItem = calc.get(calc.size() - 1);
-                        if (isOperator(lastItem)) {
-                            // El operador es inválido
-                            invalid = true;
-                        } else {
-                            // Agrega el operador
-                            calc.add(operante);
-                        }
-                    }
-
-                    if(!invalid)
-                        resultView.setText(resultView.getText().toString() + s.getText().toString()); // Limpia la pantalla
-
-                valueNumber = "";
-            });
+            s.setOnClickListener(this::onOperationButtonClick);
         }
 
 
@@ -136,6 +96,54 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // Método para manejar la entrada de números
+    public void onNumberButtonClick(View view) {
+        Button btn = (Button) view;
+        if (!resultView.getText().toString().equals("0")) {
+            resultView.setText(resultView.getText().toString() + btn.getText().toString());
+        } else {
+            resultView.setText(btn.getText().toString());
+        }
+        valueNumber += btn.getText().toString();
+    }
+
+    public void onOperationButtonClick(View view) {
+        Button button = (Button) view;
+        String operante = button.getText().toString();
+        boolean invalid = false;
+
+        if (calc.isEmpty()) {
+            // Si la lista está vacía, agrega el número seguido del operador
+            calc.add(valueNumber);
+            calc.add(operante);
+        } else if (!valueNumber.equals("")) {
+            String lastItem = calc.get(calc.size() - 1);
+            if (isOperator(lastItem)) {
+                calc.add(valueNumber);
+                calc.add(operante);
+            } else {
+                // El último elemento no es un operador, por lo que el usuario debe estar ingresando un nuevo operador
+                calc.add(operante);
+                calc.add(valueNumber);
+            }
+        } else {
+            // Verifica si el último elemento de la lista es un operador
+            String lastItem = calc.get(calc.size() - 1);
+            if (isOperator(lastItem)) {
+                // El operador es inválido
+                invalid = true;
+            } else {
+                // Agrega el operador
+                calc.add(operante);
+            }
+        }
+
+        if (!invalid) {
+            resultView.setText(resultView.getText().toString() + operante); // Limpia la pantalla
+        }
+
+        valueNumber = "";
+    }
 
     private boolean isOperator(String text) {
         return text.equals("+") || text.equals("-") || text.equals("*") || text.equals("/");
